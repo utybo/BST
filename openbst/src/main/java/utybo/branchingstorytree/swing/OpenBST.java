c639c14943caaa8be2a9a0629c7f6e489701691b
utybo <utybodev@gmail.com>
2016-12-16 19:33:06 +0100

    bst-java/OpenBST/Tutorial : Prepare for BST 1.0

--- openbst/src/main/java/utybo/branchingstorytree/swing/OpenBST.java
+++ openbst/src/main/java/utybo/branchingstorytree/swing/OpenBST.java
@@ -60,7 +60,7 @@ public class OpenBST extends JFrame
     /**
      * Version number of OpenBST
      */
-    public static final String VERSION = "0.4";
+    public static final String VERSION = "1.0";
     private static final long serialVersionUID = 1L;
 
     /**
