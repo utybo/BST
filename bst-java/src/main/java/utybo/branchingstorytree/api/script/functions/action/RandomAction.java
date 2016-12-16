583b212db52bbb25435f1e9e03b9f12c32c73b40
utybo <utybodev@gmail.com>
2016-12-16 19:24:59 +0100

    bst-java : Added rand action
    Syntax :
        rand:vartoset,maximum
        rand:vartoset,minimum,maximum
    
    (vartoset is a variable name, minimum and maximum are either variables
    or numbers)

---
+++ bst-java/src/main/java/utybo/branchingstorytree/api/script/functions/action/RandomAction.java
@@ -0,0 +1,54 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+package utybo.branchingstorytree.api.script.functions.action;
+
+import java.util.Random;
+
+import utybo.branchingstorytree.api.BSTClient;
+import utybo.branchingstorytree.api.BSTException;
+import utybo.branchingstorytree.api.script.ScriptAction;
+import utybo.branchingstorytree.api.script.VariableRegistry;
+import utybo.branchingstorytree.api.story.BranchingStory;
+
+public class RandomAction implements ScriptAction
+{
+    @Override
+    public void exec(String head, String desc, BranchingStory story, BSTClient client) throws BSTException
+    {
+        VariableRegistry registry = story.getRegistry();
+        String[] bits = desc.split(",");
+        int min = 0;
+        int max;
+        String varToSet;
+        if(bits.length == 2)
+        {
+            varToSet = bits[0];
+            max = registry.typeOf(bits[1]) == Integer.class ? (Integer)registry.get(bits[1], 0) : Integer.parseInt(bits[1]);
+        }
+        else if(bits.length == 3)
+        {
+            varToSet = bits[0];
+            min = registry.typeOf(bits[1]) == Integer.class ? (Integer)registry.get(bits[1], 0) : Integer.parseInt(bits[1]);
+            max = registry.typeOf(bits[2]) == Integer.class ? (Integer)registry.get(bits[2], 0) : Integer.parseInt(bits[2]);
+        }
+        else
+            throw new BSTException(-1, "Incorrect syntax : rand:variabletoset,maximum OR rand:variabletoset,minimum,maximum");
+        // nextInt is exclusive, we make it "inclusive" by adding 1
+        int value = new Random().nextInt(max + 1);
+        // Minimum bounds
+        value += min;
+        registry.put(varToSet, value);
+    }
+
+    @Override
+    public String[] getName()
+    {
+        return new String[] {"rand"};
+    }
+}
